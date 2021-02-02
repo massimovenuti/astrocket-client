4ab053d5519ad958504e4ae9b695d1fc4e5456f0
Evan ZARRELLA <ezarrella@etu.unistra.fr>
2021-02-02 09:40:42 +0100

    Tried things with Unity's Input Editor in order to implement custom control scheme.
    It didn't go as planned and I'll default to a library in the next commit.

---
+++ Assets/Scripts/Player/VirtualController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class VirtualController : MonoBehaviour
+{
+    private static VirtualController _instance;
+    private static VirtualController VirtualControllerInstance { get { return _instance; } }
+
+    private KeyCode _left;
+    private KeyCode _right;
+    private KeyCode _up;
+    private KeyCode _down;
+    private KeyCode _boost;
+    private KeyCode _shoot;
+
+
+    private void Awake( )
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+
+        }
+    }
+
+    public float VirtualHorizontalAxis()
+    {
+        if(Input.GetKey(_left) || Input.GetKey(_right))
+            return Input.GetAxis("Horizontal");
+        return 0f;
+    }
+
+    public float VirtualVerticalAxis()
+    {
+        if (Input.GetKey(_up) || Input.GetKey(_down))
+            return Input.GetAxis("Vertical");
+        return 0f;
+    }
+
+    public bool IsShooting()
+    {
+        if(Input.GetKey(_shoot))
+            return true;
+        return false;
+    }
+
+    public bool IsBoosting( )
+    {
+        if (Input.GetKey(_boost))
+            return true;
+        return false;
+    }
+}
