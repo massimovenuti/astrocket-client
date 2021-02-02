c4bd187b6f198816140f5cc35e682766c0075c9d
Rayan Lajarge <rayan.lajarge@etu.unistra.fr>
2021-02-02 17:37:24 +0100

    Spaceship Controller and Camera Smooth Follow

---
+++ Assets/Scripts/Player/Camera/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject _spaceShip;
+    public float _smoothTime = 0.1f;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, _spaceShip.transform.position, ref _velocity, _smoothTime);
+    }
+}
