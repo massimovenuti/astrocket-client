b6917a8074ddbd3fab6f354befc4e23084736a05
Evan ZARRELLA <ezarrella@etu.unistra.fr>
2021-02-02 17:52:56 +0100

    Beggining saving system and first draft of input manager

---
+++ Assets/Scripts/Player/Settings/SaveAndLoadSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using UnityEngine;
+using Luminosity.IO;
+using System.Xml;
+
+public class SaveAndLoadSettings
+{
+    public static void Save()
+    {
+        StringBuilder output = new StringBuilder();
+        InputSaverXML saver = new InputSaverXML(output);
+        InputManager.Save(saver);
+
+        XmlDocument xml = new XmlDocument();
+        xml.LoadXml(output.ToString());
+        Debug.Log(output.ToString());
+    }
+
+    public static void Load()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/Settings/SoundSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+public class SoundSettings
+{
+    public int MasterVolume { get; set; }
+    public int MusicVolume { get; set; }
+    public int VfxVolume { get; set; }
+
+    public string ToXML()
+    {
+        using (StringWriter sw = new StringWriter())
+        {
+            XmlSerializer serializer = new XmlSerializer(this.GetType());
+            
+        }
+        return "";
+    }
+}
