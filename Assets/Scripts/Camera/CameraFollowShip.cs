b74a7d6f4708611b042d1d8e4e7e89cbfa57c5c2
josie <midas.jos@gmail.com>
2021-02-05 22:02:21 +0100

    controls 90% ready, must adjust to cursor of rayan

---
+++ Assets/Scripts/Camera/CameraFollowShip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowShip : MonoBehaviour
+{ 
+    public GameObject _spaceShip;
+    public float SmoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, _spaceShip.transform.position, ref velocity, SmoothTime);
+    }
+}
