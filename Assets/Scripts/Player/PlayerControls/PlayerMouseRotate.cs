8f0b910ded90891ac8358780458f33423a359abb
josie <midas.jos@gmail.com>
2021-02-05 22:02:21 +0100

    controls 90% ready, must adjust to cursor of rayan

--- Assets/Scripts/Player/PlayerControls/PlayerMouseRotate.cs
+++ Assets/Scripts/Player/PlayerControls/PlayerMouseRotate.cs
@@ -18,7 +18,7 @@ public class PlayerMouseRotate : MonoBehaviour
         Vector3 positionOnScreen = mainCamera.WorldToViewportPoint(transform.position);
         Vector3 mouseOnScreen = mainCamera.ScreenToViewportPoint(Input.mousePosition);
         float angle = Mathf.Atan2(positionOnScreen.y - mouseOnScreen.y, positionOnScreen.x - mouseOnScreen.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.Euler(new Vector3(0f, -angle, 90f));
+        Quaternion rotation = Quaternion.Euler(new Vector3(0f, -angle - 90f, 0f));
 
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
