3da0515ec20b0217e2ca1067409e3cdfb82b349e
evan zarrella <ezarrella@etu.unistra.fr>
2021-02-14 00:34:15 +0100

    Rebase and merge ogAsteroidConcet onto develop
    Lots of changes and broken a few broken things
    Controls needs to be fixed.

--- Assets/Scripts/Player/Settings/SaveAndLoadSettings.cs
+++ Assets/Scripts/Player/Settings/SaveAndLoadSettings.cs
@@ -1,19 +1,12 @@
 ﻿using System.Text;
 using UnityEngine;
-using Luminosity.IO;
-using System.Xml;
+using System;
 
 public class SaveAndLoadSettings
 {
     public static void Save()
     {
-        StringBuilder output = new StringBuilder();
-        InputSaverXML saver = new InputSaverXML(output);
-        InputManager.Save(saver);
 
-        XmlDocument xml = new XmlDocument();
-        xml.LoadXml(output.ToString());
-        Debug.Log(output.ToString());
     }
 
     public static void Load()
