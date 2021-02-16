c87cdbae500a97c7d163beba621a938422cbfaa3
Theo Vuillemin <theo.vuillemin@etu.unistra.fr>
2021-02-16 15:41:06 +0100

    finished health feature

---
+++ Assets/Scripts/Player/Health/Health.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health
+{
+    public event EventHandler GetDamage;
+
+    private int health;
+    private int healthMax;
+
+    // Fonction initialisant la vie d'une instance
+    public Health(int healthMax) {
+        this.healthMax = healthMax;
+        health = healthMax;
+    }
+
+    // Fonction retournant la vie d'une instance
+    public int GetHealth() {
+        return health;
+    }
+
+    // Fonction diminuant la vie d'une instance
+    public void Damage(int damageValue) {
+        health -= damageValue;
+        if (health < 0)
+            health = 0;
+        if (GetDamage != null)
+            GetDamage(this, EventArgs.Empty);
+
+        // DEBUG
+        Debug.Log("Health : " + GetHealth());
+        if (health == 0)
+            Debug.Log("It's dead :(");
+    }
+
+    // Fonction augmentant la vie d'une instance
+    public void Heal(int healValue) {
+        health += healValue;
+        if (health > healthMax)
+            health = healthMax;
+
+        // DEBUG
+        Debug.Log("Health : " +  GetHealth());
+    }
+}
---
+++ Assets/Scripts/Player/Health/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = new Health(100);
+
+        // DEBUG
+        Debug.Log("Health : " + playerHealth.GetHealth());
+    }
+
+    // Fonction augmantant la vie d'un joueur lorsqu'il
+    // récupère un power-up de soin
+    public void PlayerHeal(int healValue)
+    {
+        // DEBUG
+        Debug.Log("Picked up power-up");
+
+        playerHealth.Heal(healValue);
+    }
+
+    // Fonction diminuant la vie du joueur lorsqu'il
+    // est touché par un laser
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            // DEBUG
+            Debug.Log("Touched by bullet");
+
+            playerHealth.Damage(40);
+        }
+    }
+}
