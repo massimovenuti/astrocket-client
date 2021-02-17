712ce8f51a29119a741f6687a796b27c7488c614
Rayan Lajarge <rayan.lajarge@etu.unistra.fr>
2021-02-17 12:09:47 +0100

    refactoring respawn manager script

--- Assets/Scripts/Map/RespawnManager.cs
+++ Assets/Scripts/Map/RespawnManager.cs
@@ -5,43 +5,41 @@ using System.Linq;
 
 public class RespawnManager : MonoBehaviour
 {
-    public string SpawnStorageTagName = "SpawnStorage";
-    public string RespawnPointName = "SpawnPoint";
-
-    public GameObject respawnPrefab;
+    public string RespawnPointStorageTagName = "RespawnPointStorage";
+    public string RespawnPointName = "RespawnPoint";
 
     public LayerMask checkLayers;
+    public float checkRadius = 25f;
+    public float yAxis = 1f;
 
-    private GameObject _respawnManager;
-
-    private IEnumerable<Vector2> spawnPoints;
     private List<GameObject> respawnPointsList;
 
-    private float height = 1f;
-    private float radius = 50f;
-    private float gridStep;
+    private GameObject _respawnManager;
+    public GameObject _enemyMockPrefab;
 
-    private float checkRadius;
+    private float _radius = 50f;
+    private float _gridStep = 50f;
+
+
+    private void Awake( )
+    {
+        GameObject go = GameObject.FindGameObjectsWithTag(RespawnPointStorageTagName).First();
+        if (go == null)
+        {
+            Debug.LogError($"There were no GameObjects with tag {RespawnPointStorageTagName} assigned self");
+        }
+        else
+        {
+            _respawnManager = go;
+        }
+
+        respawnPointsList = new List<GameObject>();
+        checkRadius = _radius / 2f;
+    }
 
     private void Start( )
     {
-        GameObject[] l = GameObject.FindGameObjectsWithTag(SpawnStorageTagName);
-        if (l.Length == 0)
-        {
-            Debug.LogError($"There were no GameObjects with tag {SpawnStorageTagName} assigned self");
-            _respawnManager = gameObject;
-        }
-        else if (l.Length >= 1)
-        {
-            _respawnManager = l.First();
-            if (l.Length > 1)
-                Debug.LogWarning($"{SpawnStorageTagName} had more than one element assigned ({l.Length})");
-        }
-        gridStep = radius;
-        checkRadius = radius / 2f;
-        respawnPointsList = new List<GameObject>();
-        spawnPoints = GetGridPointsInCircle();
-        InstantiateSpawnPoints();
+        InstantiateRespawnPoints();
         GenerateEnemiesToSpot();
     }
 
@@ -53,39 +51,40 @@ public class RespawnManager : MonoBehaviour
 
     private IEnumerable<Vector2> GetGridPointsInCircle( )
     {
-        int i1 = (int)Mathf.Ceil(-radius / gridStep);
-        int i2 = (int)Mathf.Floor(radius / gridStep);
+        int i1 = (int)Mathf.Ceil(-_radius / _gridStep);
+        int i2 = (int)Mathf.Floor(_radius / _gridStep);
 
-        float radius2 = radius * radius;
+        float radius2 = _radius * _radius;
 
         for (int i = i1; i <= i2; i++)
         {
-            float x = i * gridStep;
+            float x = i * _gridStep;
 
             float localRadius = x;
             localRadius *= localRadius;
             localRadius = (float)Mathf.Sqrt(radius2 - localRadius);
 
-            int j1 = (int)Mathf.Ceil(-localRadius / gridStep);
-            int j2 = (int)Mathf.Floor(localRadius / gridStep);
+            int j1 = (int)Mathf.Ceil(-localRadius / _gridStep);
+            int j2 = (int)Mathf.Floor(localRadius / _gridStep);
 
             for (int j = j1; j <= j2; j++)
             {
-                yield return new Vector2(x, j * gridStep);
+                yield return new Vector2(x, j * _gridStep);
             }
         }
     }
 
-    private void InstantiateSpawnPoints( )
+    private void InstantiateRespawnPoints( )
     {
         int i = 0;
+        IEnumerable<Vector2> respawnPoints = GetGridPointsInCircle();
 
-        foreach (Vector2 sp in spawnPoints)
+        foreach (Vector2 rsp in respawnPoints)
         {
             GameObject go = new GameObject(RespawnPointName + (++i));
             go.transform.parent = _respawnManager.transform;
             
-            Vector3 pos = new Vector3(sp.x, height, sp.y);
+            Vector3 pos = new Vector3(rsp.x, yAxis, rsp.y);
             go.transform.position = pos;
 
             respawnPointsList.Add(go);
@@ -94,25 +93,30 @@ public class RespawnManager : MonoBehaviour
 
     public Vector3 getSafeRespawnPoint()
     {
+        List<GameObject> l = new List<GameObject>();
         GameObject bestRespawnPoint = null;
         float maxDistance = 0f;
-        List<GameObject> l = new List<GameObject>();
+        int len = 0;
 
-        foreach (GameObject sp in respawnPointsList)
+        foreach (GameObject rsp in respawnPointsList)
         {
-            Collider[] colliders = Physics.OverlapSphere(sp.transform.position, checkRadius, checkLayers);
+            Collider[] colliders = Physics.OverlapSphere(rsp.transform.position, checkRadius, checkLayers);
 
             if (colliders.Length == 0)
             {
-                l.Add(sp);
+                l.Add(rsp);
+                len++;
             }
-            else
+            else if (len == 0)
             {
-                float minDistance = radius;
+                float minDistance = _radius;
 
                 foreach(Collider c in colliders)
                 {
-                    float d = Vector3.Distance(sp.transform.position, c.transform.position);
+                    // the distance is computed directly with the center of the collider and not the collider itself
+                    // this implies the fact it may behave strangely with objects of different size like asteroids 
+                    // we may compute it differently afterwards
+                    float d = Vector3.Distance(rsp.transform.position, c.transform.position);
                     if(d < minDistance)
                     {
                         minDistance = d;
@@ -122,15 +126,11 @@ public class RespawnManager : MonoBehaviour
                 if(minDistance > maxDistance && minDistance < checkRadius + 1f)
                 {
                     maxDistance = minDistance;
-                    bestRespawnPoint = sp;
+                    bestRespawnPoint = rsp;
                 }
             }
-
-            if (l.Count != 0)
-                break;
         }
 
-        int len = l.Count;
         GameObject go = (len != 0) ? l[Random.Range(0, len)] : bestRespawnPoint;
         Debug.Log(go.name);
 
@@ -139,23 +139,22 @@ public class RespawnManager : MonoBehaviour
 
     public void GenerateEnemiesToSpot()
     {
-        int times = 20;
+        int enemiesToSpawn = 20;
 
-        for(int i = 0; i < times; i++)
+        for(int i = 0; i < enemiesToSpawn; i++)
         {
-            Vector3 pos = new Vector3((Random.value - 0.5f) * radius * 2, 1f, (Random.value - 0.5f) * radius * 2);
-            Instantiate(respawnPrefab, pos, Quaternion.identity);
+            Vector3 pos = new Vector3((Random.value - 0.5f) * _radius * 2, 1f, (Random.value - 0.5f) * _radius * 2);
+            Instantiate(_enemyMockPrefab, pos, Quaternion.identity);
         }
     }
 
-    /*
     private void OnDrawGizmos( )
     {
-        foreach(Vector2 sp in spawnPoints)
+        if (!Application.isPlaying) return;
+
+        foreach (GameObject rsp in respawnPointsList)
         {
-            Vector3 pos = new Vector3(sp.x, height, sp.y);
-            Gizmos.DrawWireSphere(pos, checkRadius);
+            Gizmos.DrawWireSphere(rsp.transform.position, checkRadius);
         }
     }
-    */
 }
