bac3670ecd5b4a6b523730fb75583a8511a63340
evan zarrella <ezarrella@etu.unistra.fr>
2021-02-22 09:49:02 +0100

    started prototyping a UI

---
+++ Assets/Scripts/Camera/UICameraManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UICameraManager : MonoBehaviour
+{
+    Camera _mainCamera;
+    GameObject _loginPanel;
+    GameObject _mainMenuPanel;
+    GameObject _optionPanel;
+
+    private void Awake( )
+    {
+        _mainCamera = GameObject.FindGameObjectsWithTag("MainCamera").First().GetComponent<Camera>();
+    }
+
+    public void ToLoginPanel( )
+    {
+        _mainCamera.transform.LookAt(_loginPanel.transform);
+    }
+
+    public void ToMainMenu( )
+    {
+        _mainCamera.transform.rotation = Quaternion.Euler(0, 75, 0);
+    }
+
+    public void ToOptionPanel( )
+    {
+        _mainCamera.transform.LookAt(_optionPanel.transform);
+    }
+}
---
+++ Assets/Scripts/UIManager/ButtonActions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+
+
+
+public class ButtonActions : MonoBehaviour
+{
+    private UICameraManager _cam;
+    private void Awake( )
+    {
+        _cam = GameObject.Find("Camera").GetComponent<UICameraManager>();
+    }
+
+    public void OnLogin()
+    {
+        TMP_InputField mdp, user;
+        user = GameObject.Find("Username").GetComponent<TMP_InputField>();
+        mdp = GameObject.Find("Password").GetComponent<TMP_InputField>();
+
+        if(user == null || mdp == null)
+            Debug.LogError("Couldn't find password or username field");
+        else
+        {
+            // Do API call here
+            if(true)
+            {
+                _cam.ToMainMenu();
+            }
+        }        
+    }
+
+    public void OnSignIn()
+    {
+
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
