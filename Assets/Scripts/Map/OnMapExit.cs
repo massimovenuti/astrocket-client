cec09663ff05c20abfa2f938e643d86ffb57d182
Rayan Lajarge <rayan.lajarge@etu.unistra.fr>
2021-02-23 13:21:52 +0100

    destroying static/slow asteroids close to the map bounds

--- Assets/Scripts/Map/OnMapExit.cs
+++ Assets/Scripts/Map/OnMapExit.cs
@@ -8,17 +8,50 @@ public class OnMapExit : MonoBehaviour
     private void Awake( )
     {
         _rtb = GameObject.FindGameObjectsWithTag("MainCamera").First().transform.parent.GetComponent<ReturnToBattle>();
+        
+        if(_rtb == null)
+            Debug.LogError($"There were no GameObjects with tag MainCamera nor having the component ReturnToBattle");
     }
 
     private void OnTriggerEnter(Collider intruder)
     {
-        if (intruder.CompareTag("Player"))
+        if (this.CompareTag("WarningBorder") && intruder.CompareTag("Player"))
+        {
             _rtb.EnterArea();
+        }
+        else if (this.CompareTag("SmoothBorder") && intruder.CompareTag("Asteroid"))
+        {
+            intruder.GetComponent<DestroyAsteroid>().inMapBounds = true;
+        }
     }
 
     private void OnTriggerExit(Collider intruder)
     {
-        if (intruder.CompareTag("Player"))
-            _rtb.ExitArea();
+        if (this.CompareTag("WarningBorder"))
+        {
+            if (intruder.CompareTag("Player"))
+            {
+                _rtb.ExitArea();
+            }
+        }
+        else if(this.CompareTag("SmoothBorder"))
+        {
+            if (intruder.CompareTag("Asteroid"))
+            {
+                intruder.GetComponent<DestroyAsteroid>().inMapBounds = false;
+            }
+        }
+        else if(this.CompareTag("AbsoluteBorder"))
+        {
+            if (intruder.CompareTag("Player"))
+            {
+                PlayerHealth ph = intruder.GetComponent<PlayerHealth>();
+                ph.playerHealth.Damage(ph.playerHealth.GetHealth());
+            }
+            else if(intruder.CompareTag("Asteroid"))
+            {
+                Destroy(intruder.gameObject);
+            }
+        }
     }
 }
