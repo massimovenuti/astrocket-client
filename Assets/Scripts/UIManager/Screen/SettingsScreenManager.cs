22e891539298cd7920faa1944e9567477797ace1
Arthur Lejay <arthurlejay@gmail.com>
2021-04-11 18:53:29 +0200

    avancement sur l'onglet controls

--- Assets/Scripts/UIManager/Screen/SettingsScreenManager.cs
+++ Assets/Scripts/UIManager/Screen/SettingsScreenManager.cs
@@ -5,7 +5,7 @@ public class SettingsScreenManager : ScreenManager
     void Start( )
     {
 #if UNITY_ANDROID
-        Screen.orientation = ScreenOrientation.Portrait;
+        Screen.orientation = ScreenOrientation.Landscape;
 #endif
         base.Start();
     }
