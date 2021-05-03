0246d29c5525e40a7d33442aa088b1bd272a4a7a
Arthur Lejay <arthurlejay@gmail.com>
2021-05-03 20:14:46 +0200

    implémentation du nombre de joueurs

--- Assets/Scripts/Lobby/NetworkLobbyPlayer.cs
+++ Assets/Scripts/Lobby/NetworkLobbyPlayer.cs
@@ -1,27 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace Mirror.Examples.NetworkRoom
 {
     [AddComponentMenu("")]
     public class NetworkLobbyPlayer : NetworkRoomPlayer
     {
+        private int _playersNum = 0;
+        private TMP_Text _statusField;
+
         public override void OnStartClient( )
         {
             base.OnStartClient();
 
             Button leaveBtn = GameObject.Find("LeaveButton").GetComponent<Button>();
             leaveBtn.onClick.AddListener(exitLobby); // Bind button click to exitLobby()
+
+            _statusField = GameObject.Find("RoomStatus").GetComponent<TMP_Text>();
+            updateLobbyStatus();
         }
 
         public override void OnClientEnterRoom( )
         {
-            // Debug.LogFormat(LogType.Log, "OnClientEnterRoom {0}", SceneManager.GetActiveScene().path);
+            _playersNum++;
+            
         }
 
         public override void OnClientExitRoom( )
         {
-            // Debug.LogFormat(LogType.Log, "OnClientExitRoom {0}", SceneManager.GetActiveScene().path);
+            _playersNum--;
+        }
+
+        private void updateLobbyStatus()
+        {
+            _statusField.text = _playersNum.ToString() + "/8"; // TODO : ne pas hardcoder la valeur
         }
 
         /*public override void ReadyStateChanged(bool oldReadyState, bool newReadyState)
