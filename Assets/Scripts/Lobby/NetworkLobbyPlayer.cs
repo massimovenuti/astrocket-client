5abfa76a55934d27be818c186fe4f9e74c5c4204
Arthur Lejay <arthurlejay@gmail.com>
2021-05-03 18:14:02 +0200

    implémentation du bouton de déconnexion

---
+++ Assets/Scripts/Lobby/NetworkLobbyPlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mirror.Examples.NetworkRoom
+{
+    [AddComponentMenu("")]
+    public class NetworkLobbyPlayer : NetworkRoomPlayer
+    {
+        public override void OnStartClient( )
+        {
+            // Debug.LogFormat(LogType.Log, "OnStartClient {0}", SceneManager.GetActiveScene().path);
+
+            base.OnStartClient();
+
+            Button leaveBtn = GameObject.Find("LeaveButton").GetComponent<Button>();
+            leaveBtn.onClick.AddListener(exitLobby);
+        }
+
+        public override void OnClientEnterRoom( )
+        {
+            // Debug.LogFormat(LogType.Log, "OnClientEnterRoom {0}", SceneManager.GetActiveScene().path);
+        }
+
+        public override void OnClientExitRoom( )
+        {
+            // Debug.LogFormat(LogType.Log, "OnClientExitRoom {0}", SceneManager.GetActiveScene().path);
+        }
+
+        /*public override void ReadyStateChanged(bool oldReadyState, bool newReadyState)
+        {
+            // Debug.LogFormat(LogType.Log, "ReadyStateChanged {0}", newReadyState);
+        }*/
+
+        private void exitLobby()
+        {
+            GameObject.Find("RoomManager").GetComponent<AsteroidNetworkManager>().StopClient();
+        }
+    }
+}
