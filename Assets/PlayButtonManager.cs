047c15c237e0ee96ca03fbf6b181689deacee73e
Arthur Lejay <arthurlejay@gmail.com>
2021-05-07 16:54:51 +0200

    premier essai

---
+++ Assets/PlayButtonManager.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayButtonManager : MonoBehaviour
+{
+    public string ip;
+    public int port;
+
+    public string name;
+    public int numPlayers;
+    public int maxPlayers;
+
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(runGame);
+    }
+
+    void runGame( )
+    {
+        AsteroidNetworkManager go = GameObject.Find("RoomManager").GetComponent<AsteroidNetworkManager>();
+        go.networkAddress = ip;
+        go.GetComponent<IgnoranceTransport>().CommunicationPort = port;
+        go.StartClient();
+    }
+}
--- Assets/Scripts/UIManager/Screen/PlayScreenManager.cs
+++ Assets/Scripts/UIManager/Screen/PlayScreenManager.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using API.MainServer;
+using System.Collections.Generic;
+using System;
 
 public class PlayScreenManager : ScreenManager
 {
+    private GameObject _servContainer; 
+    public GameObject serverButtonPrefab;
+
     public override void Start( )
     {
 #if UNITY_ANDROID
@@ -13,6 +18,26 @@ public class PlayScreenManager : ScreenManager
 
         Button serveurButton = GameObject.Find("ServerButton").GetComponent<Button>();
         serveurButton.onClick.AddListener(runGame);
+
+        _servContainer = GameObject.Find("PlayUI").gameObject;
+    }
+
+    private void getServerList()
+    {
+        MainServerAPI mainApi = new MainServerAPI();
+        API.ServerListItem[] serverListItems = mainApi.GetServerList(); // TODO
+
+        foreach (API.ServerListItem server in serverListItems)
+        {
+            GameObject servBtn = Instantiate(serverButtonPrefab);
+            PlayButtonManager pbm = servBtn.GetComponent<PlayButtonManager>();
+            pbm.ip = server.IP;
+            pbm.port = server.Port;
+            pbm.name = server.Name;
+            pbm.numPlayers = server.PlayerCount;
+            pbm.maxPlayers = 4; // TODO
+            servBtn.transform.parent = _servContainer.transform;
+        }
     }
 
     void runGame( )
